d1a095034b237bc16bd65f5204f581b24990724b
ChristianFernandesLemos <chriscamplopes1@gmail.com>
2025-09-05 15:15:57 -0300

    Add src/Interfaces/IDatabaseConnection.cs 1757096157484

---
+++ src/Interfaces/IDatabaseConnection.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using SistemaChamados.Models;
+
+namespace SistemaChamados.Interfaces
+{
+    // Interface para operações de banco de dados
+    public interface IDatabaseConnection
+    {
+        // Métodos de conexão
+        bool AbrirConexao();
+        void FecharConexao();
+        bool TestarConexao();
+
+        // Métodos para Funcionários
+        bool InserirFuncionario(Funcionarios funcionario);
+        bool AtualizarFuncionario(Funcionarios funcionario);
+        bool RemoverFuncionario(int id);
+        Funcionarios BuscarFuncionarioPorId(int id);
+        Funcionarios BuscarFuncionarioPorEmail(string email);
+        List<Funcionarios> ListarTodosFuncionarios();
+        bool ValidarLogin(string email, string senha);
+
+        // Métodos para Chamados
+        bool InserirChamado(Chamados chamado);
+        bool AtualizarChamado(Chamados chamado);
+        bool RemoverChamado(int idChamado);
+        Chamados BuscarChamadoPorId(int idChamado);
+        List<Chamados> ListarTodosChamados();
+        List<Chamados> ListarChamadosPorTecnico(int idTecnico);
+        List<Chamados> ListarChamadosPorStatus(StatusChamado status);
+        List<Chamados> ListarChamadosPorPrioridade(int prioridade);
+
+        // Métodos de relatórios
+        DataTable ObterRelatorioGeral();
+        DataTable ObterRelatorioPorPeriodo(DateTime dataInicio, DateTime dataFim);
+    }
+}
