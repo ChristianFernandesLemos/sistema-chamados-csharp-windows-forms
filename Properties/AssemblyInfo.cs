2025b00bf242f41da5a549df5387519e9dbbc777
ChristianFernandesLemos <chriscamplopes1@gmail.com>
2025-09-05 15:15:43 -0300

    Add Properties/AssemblyInfo.cs 1757096142766

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,55 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Sistema de Chamados")]
+[assembly: AssemblyDescription("Sistema de gerenciamento de chamados técnicos desenvolvido em C# com Windows Forms")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Sua Empresa Ltda")]
+[assembly: AssemblyProduct("Sistema de Chamados")]
+[assembly: AssemblyCopyright("Copyright © 2024 Sua Empresa Ltda")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("12345678-1234-5678-9abc-123456789abc")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+
+// Additional assembly attributes for application information
+[assembly: AssemblyMetadata("Author", "Desenvolvedor do Sistema")]
+[assembly: AssemblyMetadata("Email", "dev@suaempresa.com")]
+[assembly: AssemblyMetadata("Website", "https://www.suaempresa.com")]
+[assembly: AssemblyMetadata("SupportUrl", "https://suporte.suaempresa.com")]
+[assembly: AssemblyMetadata("Documentation", "README.md")]
+
+// Security and compatibility attributes
+[assembly: System.Security.AllowPartiallyTrustedCallers]
+[assembly: System.Runtime.CompilerServices.CompilationRelaxations(CompilationRelaxations.NoStringInterning)]
+
+// Application specific metadata
+[assembly: AssemblyMetadata("DatabaseProvider", "Microsoft SQL Server")]
+[assembly: AssemblyMetadata("TargetFramework", ".NET Framework 4.7.2")]
+[assembly: AssemblyMetadata("UIFramework", "Windows Forms")]
+[assembly: AssemblyMetadata("Architecture", "Object-Oriented Programming")]
+[assembly: AssemblyMetadata("DesignPattern", "MVC (Model-View-Controller)")]
